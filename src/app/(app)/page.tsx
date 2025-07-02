895522fb36603086bdfba4f2acde39026e27ff9c
yatochka-dev <philipchef13@gmail.com>
2025-07-02 18:05:25 +0300

    fix: try and fix the build

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,7 +1,8 @@
 import Hero from "~/components/hero-section";
+import {cookies} from "next/headers";
 
 export default async function HomePage() {
-
+    await cookies()
     return (<>
         <Hero d={false}/>
     </>
