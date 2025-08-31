f8c8727384934e36716b57182cd177aa831257e8
yatochka-dev <philipchef13@gmail.com>
2025-08-31 15:18:38 +0300

    fix(db): reset migrations

--- src/migrations/20250831_121813.ts
+++ src/migrations/20250831_121813.ts
@@ -6,11 +6,14 @@ import {
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   CREATE TYPE "public"."enum_payload_jobs_log_task_slug" AS ENUM('inline', 'schedulePublish');
+   CREATE TYPE "public"."enum_contacts_inquiry_type" AS ENUM('general', 'partnership', 'speaking', 'sponsor', 'join-team', 'media');
+  CREATE TYPE "public"."enum_payload_jobs_log_task_slug" AS ENUM('inline', 'schedulePublish');
   CREATE TYPE "public"."enum_payload_jobs_log_state" AS ENUM('failed', 'succeeded');
   CREATE TYPE "public"."enum_payload_jobs_task_slug" AS ENUM('inline', 'schedulePublish');
   CREATE TYPE "public"."enum_hero_section_status" AS ENUM('draft', 'published');
   CREATE TYPE "public"."enum__hero_section_v_version_status" AS ENUM('draft', 'published');
+  CREATE TYPE "public"."enum_contact_page_status" AS ENUM('draft', 'published');
+  CREATE TYPE "public"."enum__contact_page_v_version_status" AS ENUM('draft', 'published');
   CREATE TABLE "users" (
   	"id" serial PRIMARY KEY NOT NULL,
   	"name" varchar DEFAULT 'Stealthy User' NOT NULL,
@@ -25,6 +28,49 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	"hash" varchar,
   	"login_attempts" numeric DEFAULT 0,
   	"lock_until" timestamp(3) with time zone
+  );
+  
+  CREATE TABLE "contacts" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"email" varchar NOT NULL,
+  	"inquiry_type" "enum_contacts_inquiry_type",
+  	"message" varchar NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "resources" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"title" varchar NOT NULL,
+  	"type_id" integer NOT NULL,
+  	"description" varchar NOT NULL,
+  	"content" jsonb NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "resource_types" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"label" varchar NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "media" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"alt" varchar DEFAULT '' NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"url" varchar,
+  	"thumbnail_u_r_l" varchar,
+  	"filename" varchar,
+  	"mime_type" varchar,
+  	"filesize" numeric,
+  	"width" numeric,
+  	"height" numeric,
+  	"focal_x" numeric,
+  	"focal_y" numeric
   );
   
   CREATE TABLE "payload_jobs_log" (
@@ -69,6 +115,10 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	"parent_id" integer NOT NULL,
   	"path" varchar NOT NULL,
   	"users_id" integer,
+  	"contacts_id" integer,
+  	"resources_id" integer,
+  	"resource_types_id" integer,
+  	"media_id" integer,
   	"payload_jobs_id" integer
   );
   
@@ -96,9 +146,21 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
   );
   
+  CREATE TABLE "settings_socials" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"show" boolean DEFAULT true NOT NULL,
+  	"name" varchar DEFAULT 'Twitter' NOT NULL,
+  	"icon" varchar DEFAULT 'MessageCircle' NOT NULL,
+  	"link" varchar DEFAULT 'https://example.com' NOT NULL
+  );
+  
   CREATE TABLE "settings" (
   	"id" serial PRIMARY KEY NOT NULL,
-  	"discord_community_url" varchar NOT NULL,
+  	"discord_community_url" varchar DEFAULT 'https://example.com' NOT NULL,
+  	"discord_contact_notification_webhook_url" varchar DEFAULT 'https://example.com' NOT NULL,
+  	"direct_contact_email" varchar NOT NULL,
   	"updated_at" timestamp(3) with time zone,
   	"created_at" timestamp(3) with time zone
   );
@@ -114,7 +176,8 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   
   CREATE TABLE "hero_section" (
   	"id" serial PRIMARY KEY NOT NULL,
-  	"top_badge_emoji" varchar DEFAULT '🚀',
+  	"icon" varchar,
+  	"top_badge_emoji" varchar,
   	"top_badge_text" varchar DEFAULT 'Join 1,100+ innovators worldwide',
   	"main_title" varchar DEFAULT 'UniVerse',
   	"main_subtitle" varchar DEFAULT 'Your *Launchpad* to Innovation',
@@ -139,7 +202,8 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   
   CREATE TABLE "_hero_section_v" (
   	"id" serial PRIMARY KEY NOT NULL,
-  	"version_top_badge_emoji" varchar DEFAULT '🚀',
+  	"version_icon" varchar,
+  	"version_top_badge_emoji" varchar,
   	"version_top_badge_text" varchar DEFAULT 'Join 1,100+ innovators worldwide',
   	"version_main_title" varchar DEFAULT 'UniVerse',
   	"version_main_subtitle" varchar DEFAULT 'Your *Launchpad* to Innovation',
@@ -156,18 +220,77 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	"autosave" boolean
   );
   
+  CREATE TABLE "contact_page_faq_questions" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"question" varchar,
+  	"answer" varchar
+  );
+  
+  CREATE TABLE "contact_page" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"title" varchar,
+  	"subtitle" varchar,
+  	"faq_show" boolean DEFAULT true,
+  	"_status" "enum_contact_page_status" DEFAULT 'draft',
+  	"updated_at" timestamp(3) with time zone,
+  	"created_at" timestamp(3) with time zone
+  );
+  
+  CREATE TABLE "_contact_page_v_version_faq_questions" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"question" varchar,
+  	"answer" varchar,
+  	"_uuid" varchar
+  );
+  
+  CREATE TABLE "_contact_page_v" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"version_title" varchar,
+  	"version_subtitle" varchar,
+  	"version_faq_show" boolean DEFAULT true,
+  	"version__status" "enum__contact_page_v_version_status" DEFAULT 'draft',
+  	"version_updated_at" timestamp(3) with time zone,
+  	"version_created_at" timestamp(3) with time zone,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"latest" boolean,
+  	"autosave" boolean
+  );
+  
+  ALTER TABLE "resources" ADD CONSTRAINT "resources_type_id_resource_types_id_fk" FOREIGN KEY ("type_id") REFERENCES "public"."resource_types"("id") ON DELETE set null ON UPDATE no action;
   ALTER TABLE "payload_jobs_log" ADD CONSTRAINT "payload_jobs_log_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."payload_jobs"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."payload_locked_documents"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_users_fk" FOREIGN KEY ("users_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_contacts_fk" FOREIGN KEY ("contacts_id") REFERENCES "public"."contacts"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_resources_fk" FOREIGN KEY ("resources_id") REFERENCES "public"."resources"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_resource_types_fk" FOREIGN KEY ("resource_types_id") REFERENCES "public"."resource_types"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_media_fk" FOREIGN KEY ("media_id") REFERENCES "public"."media"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_payload_jobs_fk" FOREIGN KEY ("payload_jobs_id") REFERENCES "public"."payload_jobs"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "payload_preferences_rels" ADD CONSTRAINT "payload_preferences_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."payload_preferences"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "payload_preferences_rels" ADD CONSTRAINT "payload_preferences_rels_users_fk" FOREIGN KEY ("users_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "settings_socials" ADD CONSTRAINT "settings_socials_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."settings"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "hero_section_stats" ADD CONSTRAINT "hero_section_stats_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."hero_section"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "_hero_section_v_version_stats" ADD CONSTRAINT "_hero_section_v_version_stats_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."_hero_section_v"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "contact_page_faq_questions" ADD CONSTRAINT "contact_page_faq_questions_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."contact_page"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "_contact_page_v_version_faq_questions" ADD CONSTRAINT "_contact_page_v_version_faq_questions_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."_contact_page_v"("id") ON DELETE cascade ON UPDATE no action;
   CREATE UNIQUE INDEX "users_username_idx" ON "users" USING btree ("username");
   CREATE INDEX "users_updated_at_idx" ON "users" USING btree ("updated_at");
   CREATE INDEX "users_created_at_idx" ON "users" USING btree ("created_at");
   CREATE UNIQUE INDEX "users_email_idx" ON "users" USING btree ("email");
+  CREATE INDEX "contacts_updated_at_idx" ON "contacts" USING btree ("updated_at");
+  CREATE INDEX "contacts_created_at_idx" ON "contacts" USING btree ("created_at");
+  CREATE INDEX "resources_type_idx" ON "resources" USING btree ("type_id");
+  CREATE INDEX "resources_updated_at_idx" ON "resources" USING btree ("updated_at");
+  CREATE INDEX "resources_created_at_idx" ON "resources" USING btree ("created_at");
+  CREATE INDEX "resource_types_updated_at_idx" ON "resource_types" USING btree ("updated_at");
+  CREATE INDEX "resource_types_created_at_idx" ON "resource_types" USING btree ("created_at");
+  CREATE INDEX "media_updated_at_idx" ON "media" USING btree ("updated_at");
+  CREATE INDEX "media_created_at_idx" ON "media" USING btree ("created_at");
+  CREATE UNIQUE INDEX "media_filename_idx" ON "media" USING btree ("filename");
   CREATE INDEX "payload_jobs_log_order_idx" ON "payload_jobs_log" USING btree ("_order");
   CREATE INDEX "payload_jobs_log_parent_id_idx" ON "payload_jobs_log" USING btree ("_parent_id");
   CREATE INDEX "payload_jobs_completed_at_idx" ON "payload_jobs" USING btree ("completed_at");
@@ -186,6 +309,10 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   CREATE INDEX "payload_locked_documents_rels_parent_idx" ON "payload_locked_documents_rels" USING btree ("parent_id");
   CREATE INDEX "payload_locked_documents_rels_path_idx" ON "payload_locked_documents_rels" USING btree ("path");
   CREATE INDEX "payload_locked_documents_rels_users_id_idx" ON "payload_locked_documents_rels" USING btree ("users_id");
+  CREATE INDEX "payload_locked_documents_rels_contacts_id_idx" ON "payload_locked_documents_rels" USING btree ("contacts_id");
+  CREATE INDEX "payload_locked_documents_rels_resources_id_idx" ON "payload_locked_documents_rels" USING btree ("resources_id");
+  CREATE INDEX "payload_locked_documents_rels_resource_types_id_idx" ON "payload_locked_documents_rels" USING btree ("resource_types_id");
+  CREATE INDEX "payload_locked_documents_rels_media_id_idx" ON "payload_locked_documents_rels" USING btree ("media_id");
   CREATE INDEX "payload_locked_documents_rels_payload_jobs_id_idx" ON "payload_locked_documents_rels" USING btree ("payload_jobs_id");
   CREATE INDEX "payload_preferences_key_idx" ON "payload_preferences" USING btree ("key");
   CREATE INDEX "payload_preferences_updated_at_idx" ON "payload_preferences" USING btree ("updated_at");
@@ -196,6 +323,8 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   CREATE INDEX "payload_preferences_rels_users_id_idx" ON "payload_preferences_rels" USING btree ("users_id");
   CREATE INDEX "payload_migrations_updated_at_idx" ON "payload_migrations" USING btree ("updated_at");
   CREATE INDEX "payload_migrations_created_at_idx" ON "payload_migrations" USING btree ("created_at");
+  CREATE INDEX "settings_socials_order_idx" ON "settings_socials" USING btree ("_order");
+  CREATE INDEX "settings_socials_parent_id_idx" ON "settings_socials" USING btree ("_parent_id");
   CREATE INDEX "hero_section_stats_order_idx" ON "hero_section_stats" USING btree ("_order");
   CREATE INDEX "hero_section_stats_parent_id_idx" ON "hero_section_stats" USING btree ("_parent_id");
   CREATE INDEX "hero_section__status_idx" ON "hero_section" USING btree ("_status");
@@ -205,7 +334,17 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   CREATE INDEX "_hero_section_v_created_at_idx" ON "_hero_section_v" USING btree ("created_at");
   CREATE INDEX "_hero_section_v_updated_at_idx" ON "_hero_section_v" USING btree ("updated_at");
   CREATE INDEX "_hero_section_v_latest_idx" ON "_hero_section_v" USING btree ("latest");
-  CREATE INDEX "_hero_section_v_autosave_idx" ON "_hero_section_v" USING btree ("autosave");`);
+  CREATE INDEX "_hero_section_v_autosave_idx" ON "_hero_section_v" USING btree ("autosave");
+  CREATE INDEX "contact_page_faq_questions_order_idx" ON "contact_page_faq_questions" USING btree ("_order");
+  CREATE INDEX "contact_page_faq_questions_parent_id_idx" ON "contact_page_faq_questions" USING btree ("_parent_id");
+  CREATE INDEX "contact_page__status_idx" ON "contact_page" USING btree ("_status");
+  CREATE INDEX "_contact_page_v_version_faq_questions_order_idx" ON "_contact_page_v_version_faq_questions" USING btree ("_order");
+  CREATE INDEX "_contact_page_v_version_faq_questions_parent_id_idx" ON "_contact_page_v_version_faq_questions" USING btree ("_parent_id");
+  CREATE INDEX "_contact_page_v_version_version__status_idx" ON "_contact_page_v" USING btree ("version__status");
+  CREATE INDEX "_contact_page_v_created_at_idx" ON "_contact_page_v" USING btree ("created_at");
+  CREATE INDEX "_contact_page_v_updated_at_idx" ON "_contact_page_v" USING btree ("updated_at");
+  CREATE INDEX "_contact_page_v_latest_idx" ON "_contact_page_v" USING btree ("latest");
+  CREATE INDEX "_contact_page_v_autosave_idx" ON "_contact_page_v" USING btree ("autosave");`);
 }
 
 export async function down({
@@ -215,6 +354,10 @@ export async function down({
 }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
    DROP TABLE "users" CASCADE;
+  DROP TABLE "contacts" CASCADE;
+  DROP TABLE "resources" CASCADE;
+  DROP TABLE "resource_types" CASCADE;
+  DROP TABLE "media" CASCADE;
   DROP TABLE "payload_jobs_log" CASCADE;
   DROP TABLE "payload_jobs" CASCADE;
   DROP TABLE "payload_locked_documents" CASCADE;
@@ -222,14 +365,22 @@ export async function down({
   DROP TABLE "payload_preferences" CASCADE;
   DROP TABLE "payload_preferences_rels" CASCADE;
   DROP TABLE "payload_migrations" CASCADE;
+  DROP TABLE "settings_socials" CASCADE;
   DROP TABLE "settings" CASCADE;
   DROP TABLE "hero_section_stats" CASCADE;
   DROP TABLE "hero_section" CASCADE;
   DROP TABLE "_hero_section_v_version_stats" CASCADE;
   DROP TABLE "_hero_section_v" CASCADE;
+  DROP TABLE "contact_page_faq_questions" CASCADE;
+  DROP TABLE "contact_page" CASCADE;
+  DROP TABLE "_contact_page_v_version_faq_questions" CASCADE;
+  DROP TABLE "_contact_page_v" CASCADE;
+  DROP TYPE "public"."enum_contacts_inquiry_type";
   DROP TYPE "public"."enum_payload_jobs_log_task_slug";
   DROP TYPE "public"."enum_payload_jobs_log_state";
   DROP TYPE "public"."enum_payload_jobs_task_slug";
   DROP TYPE "public"."enum_hero_section_status";
-  DROP TYPE "public"."enum__hero_section_v_version_status";`);
+  DROP TYPE "public"."enum__hero_section_v_version_status";
+  DROP TYPE "public"."enum_contact_page_status";
+  DROP TYPE "public"."enum__contact_page_v_version_status";`);
 }
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,33 +1,9 @@
-import * as migration_20250627_220641 from './20250627_220641';
-import * as migration_20250712_142734 from './20250712_142734';
-import * as migration_20250713_111503_add_inquiry_type from './20250713_111503_add_inquiry_type';
-import * as migration_20250718_202909 from './20250718_202909';
-import * as migration_20250831_120135 from './20250831_120135';
+import * as migration_20250831_121813 from './20250831_121813';
 
 export const migrations = [
   {
-    up: migration_20250627_220641.up,
-    down: migration_20250627_220641.down,
-    name: '20250627_220641',
-  },
-  {
-    up: migration_20250712_142734.up,
-    down: migration_20250712_142734.down,
-    name: '20250712_142734',
-  },
-  {
-    up: migration_20250713_111503_add_inquiry_type.up,
-    down: migration_20250713_111503_add_inquiry_type.down,
-    name: '20250713_111503_add_inquiry_type',
-  },
-  {
-    up: migration_20250718_202909.up,
-    down: migration_20250718_202909.down,
-    name: '20250718_202909',
-  },
-  {
-    up: migration_20250831_120135.up,
-    down: migration_20250831_120135.down,
-    name: '20250831_120135'
+    up: migration_20250831_121813.up,
+    down: migration_20250831_121813.down,
+    name: '20250831_121813'
   },
 ];
